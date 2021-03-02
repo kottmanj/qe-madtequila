e4642f5cbc3b81937dac957ac9e7f1dad0aa7267
Jakob S Kottmann <jakob.kottmann@gmail.com>
2021-03-02 00:16:37 -0500

    update

--- src/python/qemadtequila/_madness_tequila.py
+++ src/python/qemadtequila/_madness_tequila.py
@@ -4,9 +4,6 @@ import tequila as tq
 import os 
 
 def run_madness(geometry, n_pno, **kwargs):
-    exe = tq.quantumchemistry.QuantumChemistryMadness.find_executable()
-    print("madness exe:", exe)
-    madness_root_dir = str(os.environ.get("MAD_ROOT_DIR"))
-    print("MAD_ROOT_DIR=", madness_root_dir)
-    mol = tq.Molecule(geometry=geometry, n_pno=n_pno, **kwargs)
+    exe = tq.quantumchemistry.QuantumChemistryMadness.find_executable("/app/madroot/")
+    mol = tq.Molecule(geometry=geometry, n_pno=n_pno, executable=exe, **kwargs)
     return mol
