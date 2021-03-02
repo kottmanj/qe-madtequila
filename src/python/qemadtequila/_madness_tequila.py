c636ed83cd903b3b6dff17c3017bff3b36b39012
Jakob S Kottmann <jakob.kottmann@gmail.com>
2021-03-02 00:06:09 -0500

    update

--- src/python/qemadtequila/_madness_tequila.py
+++ src/python/qemadtequila/_madness_tequila.py
@@ -1,5 +1,5 @@
 import sys
-sys.path_append("/usr/local/lib/python3.7/dist-packages/")
+sys.path.append("/usr/local/lib/python3.7/dist-packages/")
 import tequila as tq
 
 def run_madness(geometry, n_pno, **kwargs):
