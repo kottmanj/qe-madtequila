4afcca6583ef34421f4eca40748896fdf15df8b2
AbhinavUofT <AbhinavUofT@github.com>
2021-03-08 18:53:25 -0500

    testing

--- create_tequila_molecule/dict_to_molecule.py
+++ create_tequila_molecule/dict_to_molecule.py
@@ -1,12 +1,13 @@
 import numpy as np
 import json
 
-def dict_to_molecule(geometry):
+def dict_to_molecule(molgeometry):
     geometry_str = f"{0} {1}\n"
-    for atom in geometry["sites"]:
+    for atom in molgeometry["sites"]:
         geometry_str += "{} {} {} {}\n".format(
             atom["species"], atom["x"], atom["y"], atom["z"]
         )
 
     geometry_str += "\nunits angstrom\n"
+
     print(geometry_str)
