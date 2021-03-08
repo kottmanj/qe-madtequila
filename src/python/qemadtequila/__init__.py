1e46f7c5cecb2b375f9aa0a8a2efe7d73fffcb60
Jakob S Kottmann <jakob.kottmann@gmail.com>
2021-03-08 16:29:51 -0500

    trying to improve the interface and show some standard tasks

--- src/python/qemadtequila/__init__.py
+++ src/python/qemadtequila/__init__.py
@@ -1,1 +1,1 @@
-from ._madness_tequila import run_madness
+from ._madness_tequila import run_madness, TqMadnessMoleculeEncoder, mol_to_json, mol_from_json
--- steps/run_madtequila.py
+++ steps/run_madtequila.py
@@ -2,31 +2,34 @@ import json
 import qemadtequila as qemadtq
 SCHEMA_VERSION="schema" 
 
-def run_madness(geometry, n_pno):
-    kwargs = {}
-    mol = qemadtq.run_madness(geometry=geometry, n_pno=n_pno)
+def run_madness(geometry, n_pno, **kwargs):
+
+    mol = qemadtq.run_madness(geometry=geometry, n_pno=n_pno, **kwargs)
     results_dict = {}
     results_dict["schema"] = SCHEMA_VERSION + "-madresults"
-    results_dict["info"] = """
-    Result dictionary contains data from a tequila molecule obtained with madness as backend\n
-    tequila infostring={}\n\nContent of this dictionary:\n
-    one_body_integrals:the one body integrals\n
-    two_body_integrals:the two body integrals in openfermion convention\n
-    orbitals:information about the used orbitals\n
-    nuclear_repulsion:the nuclear repulsion of the current molecule\n
-    kwargs:additional keyword arguments passed to tq.Molecule""".format(str(mol))
-    results_dict["one_body_integrals"] = mol.compute_one_body_integrals().tolist()
-    results_dict["two_body_integrals"] = mol.compute_two_body_integrals().tolist()
-    results_dict["orbitals"] = str(mol.orbitals) # workaround ... not ideal
-    results_dict["nuclear_repulsion"] = mol.molecule.nuclear_repulsion
-
-    with open("madresults.json", "w") as f:
+    results_dict["kwargs"] = kwargs
+    results_dict["geometry"] = geometry
+    results_dict["n_pno"] = n_pno
+    json_string = qemadtq.mol_to_json(mol)
+    results_dict["mol"]=json_string
+    with open("madmolecule.json", "w") as f:
         f.write(json.dumps(results_dict, indent=2))
 
-def make_qubit_operator(geometry, n_pno, transformation):
-    from zquantum.core.openfermion import save_interaction_operator # has import errors, probably issues with numpy>=1.20
-    mol = qemadtq.run_madness(geometry=geometry, n_pno=n_pno)
+def make_qubit_operator(madmolecule, transformation="JordanWigner", **kwargs):
+    # madmolecule is the result of run_madness
+    # re-initialize tq molecule
+    mol = qemadtq.mol_from_json(madmolecule, transformation="JordanWigner", **kwargs)
     H = mol.make_hamiltonian()
-    qubit_operator = H.to_openfermion()
-    save_interaction_operator(hamiltonian, "hamiltonian.json")
 
+def compute_pno_upccd(madmolcule, **kwargs):
+    # madmolecule is the result of run_madness
+    # re-initialize tq molecule
+    mol = qemadtq.mol_from_json(madmolecule, transformation="JordanWigner", **kwargs)
+    H = mol.make_hamiltonian()
+    U = mol.make_pno_upccgsd_ansatz()
+    E = tq.ExpectationValue(H=H, U=U)
+    result = tq.minimize(E, **kwargs)
+
+    energy = result.energy
+    with open("final_energy", "w") as f:
+        f.write(json_dumps(energy, indent=2))
