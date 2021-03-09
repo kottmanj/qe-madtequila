fb604ff2fb87547aabf9741844eab5a7232580a9
AbhinavUofT <AbhinavUofT@github.com>
2021-03-08 19:28:18 -0500

    testing

--- create_tequila_molecule/dict_to_molecule.py
+++ create_tequila_molecule/dict_to_molecule.py
@@ -5,4 +5,12 @@ def dict_to_molecule(molgeometry):
     geometry = None
     with open(molgeometry) as f:
         geometry = json.load(f)
-    print("here is the fucking geometery: ",geometry)
+
+    geometry_str = ""
+    for atom in geometry["sites"]:
+        geometry_str += "{} {} {} {}\n".format(
+            atom["species"], atom["x"], atom["y"], atom["z"]
+        )
+    geometry_str["schema"] = "string"
+    with open('geometry_str.json', 'w') as f:
+        f.write(json.dumps(geometry_str))
