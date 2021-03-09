0ff66d8fe084fe65b606ba6cdd5f030302b533b0
AbhinavUofT <AbhinavUofT@github.com>
2021-03-08 19:19:10 -0500

    testing

--- create_tequila_molecule/dict_to_molecule.py
+++ create_tequila_molecule/dict_to_molecule.py
@@ -2,5 +2,7 @@ import numpy as np
 import json
 
 def dict_to_molecule(molgeometry):
-    print(molgeometry)
-    
+    geometry = None
+    with open(molgeometry) as f:
+        geometry = json.load(f)
+    print("here is the fucking geometery: ",geometry)
